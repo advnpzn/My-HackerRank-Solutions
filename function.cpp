0dbd1191b1313b488ffac2c579964bbbcb086ace
Aadhavan <63637385+adenosinetp10@users.noreply.github.com>
2022-08-05 03:43:48 -0700

    Create function.cpp

---
+++ function.cpp
@@ -0,0 +1,75 @@
+// link : https://www.hackerrank.com/challenges/c-tutorial-functions/problem?h_r=next-challenge&h_v=zen&isFullScreen=true
+
+#include <iostream>
+#include <cstdio>
+using namespace std;
+
+/*
+Add `int max_of_four(int a, int b, int c, int d)` here.
+*/
+
+int max_of_four(int a, int b, int c, int d);
+
+
+int main() {
+    int a, b, c, d;
+    scanf("%d %d %d %d", &a, &b, &c, &d);
+    int ans = max_of_four(a, b, c, d);
+    printf("%d", ans);
+    
+    return 0;
+}
+
+int max_of_four(int a, int b, int c, int d)
+{
+    if (a > b) 
+    {
+        if (a > c)
+        {
+            if (a > d)
+            {
+                return a;
+            }
+            else
+            {
+                return d;
+            }
+        }
+        else 
+        {
+            if (c > d)
+            {
+                return c;
+            }
+            else   
+            {
+                return d;
+            }
+        }
+    }
+    else  
+    {
+        if (b > c)
+        {
+            if (b > d)
+            {
+                return b;
+            }
+            else  
+            {
+                return d;
+            }
+        }
+        else  
+        {
+            if (c > d)
+            {
+                return c;
+            }
+            else   
+            {
+                return d;
+            }
+        }
+    }
+}
